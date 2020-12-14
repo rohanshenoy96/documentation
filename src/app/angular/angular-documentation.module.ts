246b2fe4b75367b5baeca2d32c1b164509ed51ac
Rohan Shenoy <rshenoy@salesforce.com>
2020-12-14 13:54:33 +0530

    Added navigation basic

--- src/app/angular/angular-documentation.module.ts
+++ src/app/angular/angular-documentation.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
 
 import { AngularDocumentationRoutingModule } from './angular-documentation-routing.module';
 import { MaterialModule } from './../material.module';
 
 import { AngularDocumentation } from './angular-documentation.component';
-import { TitlePlaceholderComponent, ToolbarComponent, FooterComponent } from './index';
-import { GettingStartedComponent } from './documents/getting-started/getting-started.component';
-import { BasicConceptsComponent } from './documents/basic-concepts/basic-concepts.component';
+import { TitlePlaceholderComponent, ToolbarComponent, FooterComponent, GettingStartedComponent, BasicConceptsComponent } from './index';
+import { SidenavAccordionComponent } from './sidenav-accordion/sidenav-accordion.component';
+import { NavigationService } from './services/navigation.service';
 
 
 @NgModule({
@@ -17,10 +18,15 @@ import { BasicConceptsComponent } from './documents/basic-concepts/basic-concept
         FooterComponent,
         GettingStartedComponent,
         BasicConceptsComponent,
+        SidenavAccordionComponent,
     ],
     imports: [
+        CommonModule,
         MaterialModule,
         AngularDocumentationRoutingModule
+    ],
+    providers: [
+        NavigationService
     ],
     exports: []
 })
