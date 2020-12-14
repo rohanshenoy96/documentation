014dc959540e50216b810a949031a26c84b9e968
Rohan Shenoy <rshenoy@salesforce.com>
2020-12-14 22:02:42 +0530

    Added content to getting started page

--- src/app/angular/title-placeholder/title-placeholder.component.ts
+++ src/app/angular/title-placeholder/title-placeholder.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title-placeholder',
   templateUrl: './title-placeholder.component.html',
   styleUrls: ['./title-placeholder.component.scss']
 })
-export class TitlePlaceholderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class TitlePlaceholderComponent {
+  @Input() title: string;
 
 }
